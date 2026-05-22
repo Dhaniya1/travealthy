46fc1f1fc22109ab738717f8ad07b9e02686d2bf
Dhaniya1 <ndohare11@gmail.com>
2026-05-23 04:11:14 +0530

    added database

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+server_password = os.getenv("SUPABASE_PASS")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres.xldoknfkntxvhrtsdtuy:{server_password}@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, echo = True
+)
+
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit= False)
+
+class Base(DeclarativeBase):
+    pass
